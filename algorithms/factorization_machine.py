1d8512e6adb7781908696c6290568ffc5f0c0173
Farouq Oyebiyi <farouq.oyebiyi@adroyte.com>
2017-04-04 07:20:40 +0100

    initialized als model with some default params

--- algorithms/factorization_machine.py
+++ algorithms/factorization_machine.py
@@ -5,8 +5,11 @@ class FactorizationMachine():
 A wrapper around an implementation of Factorization Machines
 '''
 
-	def fit();
+	def __init__(self):
+		self.model = als.FMRegression(n_iter=1000, init_stdev=0.1, rank=2, l2_reg_w=0.1, l2_reg_V=0.5)
 
-	def predict():
+	def fit(self, features, target);
+
+	def predict(self, features):
 
 
